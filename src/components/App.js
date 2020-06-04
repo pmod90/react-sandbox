2da0c5df4af33a9041a255d0f0f6c82d7edc58c6
pmod90 <parth.mody@4cinsights.com>
2020-06-03 21:25:44 -0700

    adding components and setting up router

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import {Route} from 'react-router-dom';
 import homePage from './home/homePage'
 import AboutPage from './about/AboutPage';
+import Header from './common/header';
 
 function App() {
   return (
     <div className="container-fluid">
+      {/* will always render */}
+      <Header />
+       {/* will render depending on the path in url */}
       <Route exact path="/" component={homePage} />
       <Route exact path="/about" component={AboutPage} />
 
